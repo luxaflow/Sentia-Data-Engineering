7ee9147a9b487900585bc6c3e88fcf3cde3d610f
Lucas.Wolfe <lucas.wolfe@sltn.nl>
2021-06-04 00:08:13 +0200

    added keyvault variables

---
+++ tf-resources/keyvault.tfvars
@@ -0,0 +1,11 @@
+keyvault_rg_name = "customer-keyvault-rg-name"
+
+keyvault_name = "customer-keyvault-name"
+
+keyvault_sql_server_username = "customer-username-secret"
+
+keyvault_sql_server_password = "customer-password-secret"
+
+keyvault_aad_object_id = "customer-aad-object-id-secret"
+
+keyvault_aad_username = "customer-aad-username"
--- tf-resources/variables.tf
+++ tf-resources/variables.tf
@@ -29,6 +29,42 @@ variable "network_rg_name" {
   description = "Name of resouce group for VPN Gateway and its resources"
 }
 
+// Key Vault
+
+variable "keyvault_rg_name" {
+  type    = string
+  description = "Name of the resource group containing the required keyvault"
+}
+
+variable "keyvault_name" {
+  type        = string
+  description = "Name of the required keyvault"
+}
+
+variable "keyvault_sql_server_username" {
+  type        = string 
+  
+  sensitive   = true
+}
+
+variable "keyvault_sql_server_password" {
+  type    = string
+  description = "Name for secret for sql server password"
+  sensitive   = true
+}
+
+variable "keyvault_aad_object_id" {
+  type = string
+  description = "Name for secret for azure active directory object id"
+  sensitive   = true
+}
+
+variable "keyvault_aad_username" {
+  type    = string
+  description = "Name for secret for azure active directory username"
+  sensitive   = true
+}
+
 // Data Factory
 
 variable "data_factory_name" {
