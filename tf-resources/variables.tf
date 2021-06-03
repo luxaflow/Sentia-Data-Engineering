db06b4801d65e538711dd127c26b8aa46f4dd820
Lucas.Wolfe <lucas.wolfe@sltn.nl>
2021-06-03 23:38:57 +0200

    added regions & etl_rg

--- tf-resources/variables.tf
+++ tf-resources/variables.tf
@@ -6,8 +6,22 @@ variable "env" {
 }
 
 variable "main_location" {
-  type    = string
-  default = "westeurope"
+  type        = string
+  default     = "westeurope"
+  description = "Main region location to be used for resources"
+}
+
+varaiable "fallback_location" {
+  type        = string
+  default     = "northeurope"
+  description = "Fallback region location to be used for resources, region pair with westeurope"
+}
+
+// Resource Groups
+
+variables "etl_rg" {
+  type = string
+  description = "Name of resouce group for Azure Data Factory and its resources"
 }
 
 // Data Factory Variables
